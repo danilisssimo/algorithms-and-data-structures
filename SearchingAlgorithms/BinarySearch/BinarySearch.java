af81a29c0bc42d675fff40a5552c16b6d0ae9a68
Данила Потапчук <potapchuk01@mail.ru>
2025-02-13 15:11:24 +0300

    Complete Binary Search

--- SearchingAlgorithms/BinarySearch/BinarySearch.java
+++ SearchingAlgorithms/BinarySearch/BinarySearch.java
@@ -7,15 +7,16 @@ public class BinarySearch {
         int rightIndex = array.length - 1;
         int middleIndex = array.length/2;
         while (leftIndex <= rightIndex) {
-            if (array[middleIndex] == searching) {
-                return middleIndex;
-            } else if (array[middleIndex] > searching) {
-                rightIndex = middleIndex;
-                middleIndex = (rightIndex - leftIndex)/2;
-            } else {
-                leftIndex = middleIndex;
-                middleIndex = (rightIndex + leftIndex)/2;
-            }
+            if (array[middleIndex] == searching) return middleIndex;
+
+            if (array[middleIndex] > searching) rightIndex = middleIndex; 
+            else leftIndex = middleIndex;
+
+            int diffOfIndexies = rightIndex - leftIndex;
+            if (diffOfIndexies > 1) middleIndex = (diffOfIndexies)/2 + leftIndex;
+            else if (array[leftIndex] == searching) return leftIndex;
+            else if (array[rightIndex] == searching) return rightIndex;
+            else break;
         }
         return -1;
     }
