182be1e3d6670e470e63fd4353688e873f6b708d
Данила Потапчук <potapchuk01@mail.ru>
2025-02-20 16:18:39 +0300

    Add tests

--- DataStructures/TreeStructure/main.go
+++ DataStructures/TreeStructure/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	tests "main/Tree"
+	tree "main/Tree"
 	"main/bst"
 )
 
-func main() {
+func testForBST() {
 	var HeadNode bst.BSTNode = bst.BSTNode{Value: 0, Right: nil, Left: nil}
 	for index := -10; index < 11; index += 2 {
 		err := HeadNode.Append(index)
@@ -17,4 +19,26 @@ func main() {
 		fmt.Println("]")
 		fmt.Printf("Max: %d, Min: %d\n", HeadNode.FindMax(), HeadNode.FindMin())
 	}
+}
+
+func testForTree() {
+	var arrOfTrees []*tree.Node = []*tree.Node{
+		tests.Tree1,
+		tests.Tree2,
+		tests.Tree3,
+		tests.Tree4,
+		tests.Tree5,
+		tests.Tree6,
+		&tree.Node{Value: 1},
+	}
+	for _, headNode := range arrOfTrees {
+		fmt.Printf("Tree: ")
+		tree.PrintTree(headNode)
+		fmt.Printf("\nHeight of node: %d\n", headNode.GetHeight())
+	}
+}
+
+func main() {
+	testForBST()
+	testForTree()
 }
