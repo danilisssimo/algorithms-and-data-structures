fb712939d8f634d8cec2bf73648452577b5a2619
Данила Потапчук <potapchuk01@mail.ru>
2025-02-24 15:17:52 +0300

    Refactoring

--- Algorithms/SearchingAlgorithms/BreadthFirstSearch/tree/tree.go
+++ Algorithms/SearchingAlgorithms/BreadthFirstSearch/tree/tree.go
@@ -1,9 +1,15 @@
-package tree
+package BFSTree
+
+import (
+	tree "algorithms-and-data-structures/DataStructures/TreeStructure/Tree"
+)
 
 type Node struct {
-	Value int
-	Left  *Node
-	Right *Node
+	*tree.Node
 }
 
-func (node *Node) FindNode(value int) {}
+func (node *Node) FindNode(value int) *tree.Node {
+	return findNode(node.Node, value)
+}
+
+func findNode(node *tree.Node, value int) *tree.Node { return node }
