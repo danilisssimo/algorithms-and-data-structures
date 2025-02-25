00b0382d3812b0b0a96c906bb94426c82cf7a148
Данила Потапчук <potapchuk01@mail.ru>
2025-02-25 16:24:26 +0300

    complete breadth-first search

--- Algorithms/SearchingAlgorithms/BreadthFirstSearch/tree/tree.go
+++ Algorithms/SearchingAlgorithms/BreadthFirstSearch/tree/tree.go
@@ -1,7 +1,9 @@
 package BFSTree
 
 import (
+	fifo "algorithms-and-data-structures/DataStructures/Stack/FIFO"
 	tree "algorithms-and-data-structures/DataStructures/TreeStructure/Tree"
+	"fmt"
 )
 
 type Node struct {
@@ -12,4 +14,26 @@ func (node *Node) FindNode(value int) *tree.Node {
 	return findNode(node.Node, value)
 }
 
-func findNode(node *tree.Node, value int) *tree.Node { return node }
+func findNode(node *tree.Node, value int) *tree.Node {
+	var result *tree.Node = nil
+	queue := fifo.GetNewQueue(1)
+	if node == nil {
+		return result
+	}
+	queue.Enqueue(node)
+	for queue.GetLength() > 0 {
+		queueNode := queue.Dequeue().(*tree.Node)
+		fmt.Println(queueNode.Value)
+		if queueNode.Value == value {
+			result = queueNode
+			break
+		}
+		if queueNode.Left != nil {
+			queue.Enqueue(queueNode.Left)
+		}
+		if queueNode.Right != nil {
+			queue.Enqueue(queueNode.Right)
+		}
+	}
+	return result
+}
