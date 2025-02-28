4c4901c64c70db4990bfeee498bcbf474e6aab19
Данила Потапчук <potapchuk01@mail.ru>
2025-02-28 13:10:40 +0300

    Add draw test for linked list

--- DataStructures/Lists/main.go
+++ DataStructures/Lists/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	linkedlist "algorithms-and-data-structures/DataStructures/Lists/LinkedList"
+)
+
+func main() {
+	testForLinkedList()
+}
+
+func testForLinkedList() {
+	linkedlist.List1.Draw()
+	linkedlist.List2.Draw()
+	linkedlist.List3.Draw()
+	linkedlist.List4.Draw()
+}
