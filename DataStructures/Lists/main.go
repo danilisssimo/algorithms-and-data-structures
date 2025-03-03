288d4130061c9aec53e8575fa6a5978a7130e4b6
Данила Потапчук <potapchuk01@mail.ru>
2025-03-03 22:40:06 +0300

    Add new tests

--- DataStructures/Lists/main.go
+++ DataStructures/Lists/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	linkedlist "algorithms-and-data-structures/DataStructures/Lists/LinkedList"
+	"fmt"
 )
 
 func main() {
 	testForLinkedList()
+	testPrependForLinkedList()
+	testRemoveForLinkedList()
 }
 
 func testForLinkedList() {
@@ -14,5 +17,28 @@ func testForLinkedList() {
 	newList.Append("World")
 	newList.Append("!")
 	newList.Append(677)
+	newList.Draw()
+}
+
+func testPrependForLinkedList() {
+	var newList *linkedlist.List = linkedlist.CreateLinkedList([]interface{}{})
+	newList.Append("Hello")
+	newList.Prepend("World")
+	newList.Append("!")
+	newList.Prepend(677)
+	newList.Draw()
+}
+
+func testRemoveForLinkedList() {
+	var newList *linkedlist.List = linkedlist.List1
+	newList.Draw()
+	fmt.Println("Delete 1")
+	newList.RemoveFirst()
+	newList.Draw()
+	fmt.Println("Delete 4")
+	newList.RemoveFirst()
+	newList.RemoveFirst()
+	newList.RemoveFirst()
+	newList.RemoveFirst()
 	newList.Draw()
 }
