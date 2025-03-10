f8171572b4cebc6c92a97d07049b3270dbb6cb05
Данила Потапчук <potapchuk01@mail.ru>
2025-03-10 20:41:20 +0300

    init utils

---
+++ ProblemSolving/CustomTasks/BitwiseOperation/utils/util.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func PrintAsBinary(a any) {
+	type iface struct {
+		t, v unsafe.Pointer
+	}
+	p := uintptr((*(*iface)(unsafe.Pointer(&a))).v)
+
+	t := reflect.TypeOf(a)
+
+	for i := 0; i < int(t.Size()); i++ {
+		n := *(*byte)(unsafe.Pointer(p))
+		fmt.Printf("%08b ", n)
+		p += unsafe.Sizeof(n)
+	}
+	fmt.Printf("\n")
+}
