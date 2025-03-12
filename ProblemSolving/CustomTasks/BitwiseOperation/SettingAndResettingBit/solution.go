d7ad434e37964580bc65107ae046a788c931d9d6
Данила Потапчук <potapchuk01@mail.ru>
2025-03-12 14:07:35 +0300

    Complete solution

--- ProblemSolving/CustomTasks/BitwiseOperation/SettingAndResettingBit/solution.go
+++ ProblemSolving/CustomTasks/BitwiseOperation/SettingAndResettingBit/solution.go
@@ -1,21 +1,11 @@
 package settingandresettingbit
 
 func Solution(number int, index int, value bool) int {
-	currentValue := 0
 	if value {
-		currentValue = 1
+		number |= 1 << index
+	} else {
+		mask := ^(1 << index)
+		number &= mask
 	}
-	count := 0
-	temp := 0
-	for number != 0 {
-		if count == index {
-			number |= number & currentValue
-		}
-		temp |= number & 1
-		temp <<= 1
-		number >>= 1
-		count += 1
-	}
-	temp >>= 1
-	return temp
+	return number
 }
