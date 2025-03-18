789735b16da7ab84acd5fdae0d394037b0f74d58
Данила Потапчук <potapchuk01@mail.ru>
2025-03-18 15:43:59 +0300

    Complete minrun

--- Algorithms/SortingAlgorithms/TimSort/sort.go
+++ Algorithms/SortingAlgorithms/TimSort/sort.go
@@ -1,6 +1,31 @@
 package timsort
 
-func Sorted(arr []int) {}
+func Sorted(arr []int) []int {
+	var sortedSlice []int
+	sliceLen := len(arr)
+	minRun := getMinrun(sliceLen)
+	var index int = 0
+	for index <= sliceLen {
+		var subIndex int = index
+		// Here insertion sort
+		for subIndex <= minRun+subIndex {
+			if arr[subIndex] > arr[subIndex+1] {
+
+			}
+		}
+		// Here merging
+	}
+}
+
+func insertionSort(slice []int) {}
+func merging(slice []int) []int {}
 
 // Find min subarray length for Timsort
-func getMinrun(n int) {}
+func getMinrun(n int) int {
+	var lastBit int = 0
+	for n <= 64 {
+		lastBit |= n & 1
+		n >>= 1
+	}
+	return n + lastBit
+}
